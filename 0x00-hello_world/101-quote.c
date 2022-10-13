d5f78074ec8f33f3b453236b3e6120bc064d17cf
nanasei0548 <nanasei0548@gmail.com>
2022-10-13 08:06:24 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <unistd>
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - Entry
+ * Return: 1 
  */
 
 int main(void)
-
 {
-	write(STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 
  	return (1);
 
