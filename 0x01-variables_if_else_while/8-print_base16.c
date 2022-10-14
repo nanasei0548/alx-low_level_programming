8153185dc6373d0cf56c83bbc76fbd16b4e21fb6
nanasei0548 <nanasei0548@gmail.com>
2022-10-14 16:02:24 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib>
+#include<time.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a:
+	char b;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+	for (b = 'x'; b <= 'y'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
