67eb4b3106aae0e8537c0e7509264d8ac62da1fc
nanasei0548 <nanasei0548@gmail.com>
2022-10-14 16:23:45 -0700

    patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int d;
 
-	for (d = '0';d <= '9'; d++)
+	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
 		if (d != '9')
@@ -21,5 +21,6 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return(0);
+
+	return (0);
 }
