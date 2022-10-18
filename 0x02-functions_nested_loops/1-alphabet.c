9c434ee2006d9d09d5e39ad0bfc4d1de63bac7e8
nanasei0548 <nanasei0548@gmail.com>
2022-10-18 10:38:36 -0700

    abc

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/** print_alphabet - function to print abc
+/**
+ * print_alphabet - function to print abc
  * Return: 0
  */
 void print_alphabet(void)
@@ -10,5 +11,5 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
-	_putchar("\n")
+	_putchar('\n');
 }
