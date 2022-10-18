57fdce672d61a17211e50c2c2723e4fdf4354818
nanasei0548 <nanasei0548@gmail.com>
2022-10-18 06:29:10 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include "holberton.h"
 
 /**
  * main - Check description
@@ -11,10 +10,9 @@ int main(void)
 {
 	char word[9] = "Holberton";
 	int i;
-	
+
 	for (i = 0; i < 9; i++)
 		_putchar(word[i]);
 	_putchar('\n');
-	
 	return (0);
 }
