17ec5b4be77ed5155f957a34f5394896378f67f0
nanasei0548 <nanasei0548@gmail.com>
2022-10-18 13:48:39 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * _abs - prints absolute value of an integer
+ * @x: integer required
+ * Return: absolute value
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (-x);
+}
