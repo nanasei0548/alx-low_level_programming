1c760be1c47429762320236e676656679020c51b
nanasei0548 <nanasei0548@gmail.com>
2022-10-20 11:42:15 -0700

    dont print 2 and 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,7 +10,8 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-
+int _putchar(char c);
+void print_triangle(int size);
 void print_triangle(int size);
 
 void print_number(int n);
