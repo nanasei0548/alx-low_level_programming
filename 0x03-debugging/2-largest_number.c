7648034a1e45f0880ac2147ae7b4ae04eb0ba7fd
nanasei0548 <nanasei0548@gmail.com>
2022-10-20 13:29:21 -0700

    multiple mains

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints the largest of 3 integers
+* Return: 0
+* @x: integer one
+* @y: integer two
+* @z: integer three
+*/
+
+int largest_number(int x, int y, int z)
+{
+	int largest
+
+	if (x > y)
+	{
+		if (y > z)
+			largest = x;
+															else if (x > z)
+			largest = x;
+		else 
+			largest = z;
+	else;
+		if (y < z)
+			largest = z;
+		else
+			largest = y;
+	}
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,17 @@
+#ifdef MAIN_H
+#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+
+
+
+#endif
+
+
+
