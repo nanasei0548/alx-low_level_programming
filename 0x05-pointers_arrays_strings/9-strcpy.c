33d4fb02bbf316cfae37cbed4be4105245ebeccc
nanasei0548 <nanasei0548@gmail.com>
2022-10-24 19:28:06 +0000

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,7 +14,7 @@ char *_strcpy(char *dest, char *src)
 
 	while (*(src + index) != '\0')
 	{
-		*(dest + index) = *(src +index);
+		*(dest + index) = *(src + index);
 		index++;
 	}
 	*(dest + index) = '\0';
