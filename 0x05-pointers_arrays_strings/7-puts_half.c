c6d6c3ad119a5d3a18cf0afac0c684f5ca185867
nanasei0548 <nanasei0548@gmail.com>
2022-10-24 18:43:49 +0000

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "nana.h"
+
+/**
+ * puts_half - a function that prints half of a string, followed by a new line.
+ * @str: An input string
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int len = 0, u, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len + 1) / 2;
+
+	for (i = n; u < len; u++)
+		_putchar(str[u]);
+
+	_putchar('\n');
+}
