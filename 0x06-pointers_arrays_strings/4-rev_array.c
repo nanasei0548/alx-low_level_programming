598dd62faf3d9cd405c88cb1b36a306c0c1d9aa6
nanasei0548 <nanasei0548@gmail.com>
2022-10-26 18:35:42 +0000

    solution

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "nana.h"
+
+/**
+ * reverse_array - a function that reverse an array.
+ * @a: an input array
+ * @n: number of elements of array
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int rev;
+
+	while (i < n--)
+	{
+		rev = a[i];
+		a[i++] = a[n];
+		a[n] = rev;
+	}
+}
