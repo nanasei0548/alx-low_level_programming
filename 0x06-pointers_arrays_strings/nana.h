b6bb41b3d001e995c5c97e8728d2ad1444b57dbd
nanasei0548 <nanasei0548@gmail.com>
2022-10-27 10:00:31 +0000

    fix

--- 0x06-pointers_arrays_strings/nana.h
+++ 0x06-pointers_arrays_strings/nana.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -14,6 +15,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int base10(int n);
 
 #endif
