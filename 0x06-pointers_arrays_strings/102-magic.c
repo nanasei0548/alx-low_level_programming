e86740b7ef8e796e65e27bbddbb2f68b0f1d33d5
nanasei0548 <nanasei0548@gmail.com>
2022-10-27 11:00:49 +0000

    fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/*
+ * write your line of code here...
+ * Remember:- 
+ * you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
+ */
 int main(void)
 {
 	int n;
