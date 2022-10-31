f211c9b0448a0e354a47684016e798c08173e1e5
nanasei0548 <nanasei0548@gmail.com>
2022-10-31 20:06:14 +0000

    fixed

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,7 +7,7 @@
  * @n: number of bytes to fill
  * Return: returns pointer to location filled
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char c, unsigned int n)
 {
 	char *start = s;
 
