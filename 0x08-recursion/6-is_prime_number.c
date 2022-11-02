85dd0014277104ba54acf2894d75a07b72f255c5
nanasei0548 <nanasei0548@gmail.com>
2022-11-02 09:58:44 +0000

    solutions

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,15 +1,29 @@
 #include "nana.h"
-
 int is_divisible(int num, int div);
-int is_prime_number(int n);
 
 /**
- * is_divisible - Checks if a number is divisible.
- * @num: The number to be checked.
- * @div: The divisor.
- *
- * Return: If the number is divisible - 0.
- *         If the number is not divisible - 1.
+ * is_prime_number - Afunction that checks if a number is prime.
+ * @n: an input integer
+ * Return: 1 if n is prime or  0 in otherwise
+ */
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n <= 1)
+		return (0);
+
+	if (n <= 3)
+		return (1);
+
+	return (is_divisible(n, div));
+}
+
+/**
+ * is_divisible - check if num is divisible
+ * @num: the number to be checked
+ * @div: the result of division
+ * Return: 1 if num is divisible or 0 if numis not divisible
  */
 int is_divisible(int num, int div)
 {
@@ -21,23 +35,3 @@ int is_divisible(int num, int div)
 
 	return (is_divisible(num, div + 1));
 }
-
-/**
- * is_prime_number - Checks if a number is prime.
- * @n: The number to be checked.
- *
- * Return: If the integer is not prime - 0.
- *         If the number is prime - 1.
- */
-int is_prime_number(int n)
-{
-	int div = 2;
-
-	if (n <= 1)
-		return (0);
-
-	if (n >= 2 && n <= 3)
-		return (1);
-
-	return (is_divisible(n, div));
-
