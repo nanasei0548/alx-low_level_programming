303fbdc6bb64aa9faf02297eb75a4e02eeae23e5
nanasei0548 <nanasei0548@gmail.com>
2022-11-07 19:07:44 +0000

    deep

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,32 +1,45 @@
-#include "nana.h"
 #include <stdlib.h>
+#include "nana.h"
 
 /**
- * alloc_grid - A function that returns a pointer to a
- * 2 dimensional array of integers
- * @width: An input integer at number of columns
- * @height: An input integer at number of rows
- * Return: pointer to a 2D array, NULL on failure
+ * **alloc_grid - creates a two dimensional array of ints
+ * @width: width of the matrix
+ * @height: height of the matrix
+ *
+ * Return: pointer to the created matrix (Success)
+ * or NULL (Error)
  */
 int **alloc_grid(int width, int height)
 {
-	int **array, i = 0, j = 0;
+	int **array;
+	int i, j;
 
-	if (width <= 0 || height <= 0)
+	if (height <= 0 || width <= 0)
 		return (NULL);
 
-	array = (int **)malloc(sizeof(int *) * height);
+	array = (int **) malloc(sizeof(int *) * height);
+
 	if (array == NULL)
 		return (NULL);
-	for (; i < height; i++)
+
+	for (i = 0; i < height; i++)
 	{
-		array[i] = (int *)malloc(sizeof(int) * width);
+		array[i] = (int *) malloc(sizeof(int) * width);
 		if (array[i] == NULL)
+		{
+			free(array);
+			for (j = 0; j <= i; j++)
+				free(array[j]);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
-		for (; j < width; j++)
+	{
+		for (j = 0; j < width; j++)
+		{
 			array[i][j] = 0;
+		}
+	}
 	return (array);
 }
